330fc05f353a3ae8ef00ba2e0c219bffe8f1e64f
hipogrifo010 <46372474+hipogrifo010@users.noreply.github.com>
2022-11-30 13:11:26 -0300

    Feature-Repository-UnitOfWork
    Base Repository and unit of work

---
+++ Repositories/UnitOfWork.cs
@@ -0,0 +1,39 @@
+﻿using ApiSalud.DataAccess;
+using ApiSalud.Entities;
+using ApiSalud.Repositories.Interfaces;
+
+namespace ApiSalud.Repositories;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly SaludContext _dbContext;
+    private IRepositoryBase<Product>? _productRepository;
+    private IProductRepository _productWithDetails;
+
+
+    public UnitOfWork(SaludContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+
+    public IRepositoryBase<Product> ProductRepository => _productRepository ?? new RepositoryBase<Product>(_dbContext);
+    public IProductRepository ProductWithDetails => _productWithDetails ?? new ProductRepository(_dbContext);
+
+
+    public void Dispose()
+    {
+        _dbContext.Dispose();
+        GC.SuppressFinalize(this);
+    }
+
+    public int SaveChanges()
+    {
+        return _dbContext.SaveChanges();
+    }
+
+    public async Task<int> SaveChangesAsync()
+    {
+        return await _dbContext.SaveChangesAsync();
+    }
+}
