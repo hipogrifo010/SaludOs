232f4bd976bacbe9f7547d54fec8db7e63cec91d
hipogrifo010 <46372474+hipogrifo010@users.noreply.github.com>
2022-11-30 12:58:38 -0300

    Feature-Repository-Generic-and-UnitOfWork
    Include Unit Of Work

---
+++ Repositories/Interfaces/IProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using ApiSalud.Entities;
+
+namespace ApiSalud.Repositories.Interfaces;
+
+public interface IProductRepository : IRepositoryBase<Product>
+{
+    Task<IEnumerable<Product>> GetAll();
+
+    Task<(int totalPages, IEnumerable<Product> recordList)> GetProductPagingextra(int userId, int pageNumber,
+        int pageSize);
+}
---
+++ Repositories/Interfaces/IRepositoryBase.cs
@@ -0,0 +1,11 @@
+﻿namespace ApiSalud.Repositories.Interfaces;
+
+public interface IRepositoryBase<T> where T : class
+{
+    Task<(int totalPages, IEnumerable<T> recordList)> GetAllPaging(int pageNumber, int pageSize);
+    Task<IEnumerable<T>> GetAll();
+    Task<T> GetById(int id);
+    Task Insert(T entity);
+    Task Update(T entity);
+    Task Delete(int id);
+}
