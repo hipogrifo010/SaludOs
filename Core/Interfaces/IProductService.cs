6ce164285dbbb5fd5e13fbdd0b3fcc7c80fb4108
hipogrifo010 <46372474+hipogrifo010@users.noreply.github.com>
2022-11-30 13:16:06 -0300

    Feature-Services
    Product services

---
+++ Core/Interfaces/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using ApiSalud.Core.Models.DTO;
+using ApiSalud.Entities;
+
+namespace ApiSalud.Core.Interfaces;
+
+public interface IProductService
+{
+    Task<bool> DeleteProduct(int id);
+    Task<bool> UpdateDeposit(int id, ProductDto productDto);
+    Task InsertProduct(ProductDto productDto);
+    Task<(int totalPages, IEnumerable<Product> recordList)> GetProductsPagingdos(int pageNumber, int pageSize);
+
+    Task<IEnumerable<Product>> GetProducts();
+}
