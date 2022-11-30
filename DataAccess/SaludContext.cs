02395194d790b639655a730fa6f412b1083fe390
hipogrifo010 <46372474+hipogrifo010@users.noreply.github.com>
2022-11-30 12:54:43 -0300

    Feature-Database epica
    Include Context and Entities

---
+++ DataAccess/SaludContext.cs
@@ -0,0 +1,24 @@
+﻿using ApiSalud.DataAccess.Seeds;
+using ApiSalud.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiSalud.DataAccess;
+
+public class SaludContext : IdentityDbContext<ApplicationUser>
+{
+    public SaludContext(DbContextOptions<SaludContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Product>? Products { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        new ProductSeeder(builder).SeedProduct();
+
+        builder.Entity<Product>().ToTable("Product");
+        base.OnModelCreating(builder);
+    }
+}
---
+++ Entities/Product.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ApiSalud.Entities;
+
+[Table("Product")]
+public class Product
+{
+    [Key] public int Id { get; set; }
+
+    [Required(ErrorMessage = "Enter a Valid Product Name")]
+    [Column("ProductName", TypeName = "varchar")]
+    [MaxLength(100)]
+    public string ProductName { get; set; } = "none";
+
+    [Range(0, 9999999999999)]
+    [Required(ErrorMessage = "An Amount its Required")]
+    public int Price { get; set; }
+
+    [Required(ErrorMessage = "Enter a Valid Product Name")]
+    [Column("DrugBrand", TypeName = "varchar")]
+    [MaxLength(100)]
+    public string DrugBrand { get; set; } = "none";
+
+    [Required(ErrorMessage = "Enter a Valid Product Name")]
+    [Column("Company", TypeName = "varchar")]
+    [MaxLength(100)]
+    public string Company { get; set; } = "none";
+
+    [Required(ErrorMessage = "Enter a Valid Product Name")]
+    [Column("TypeOfMedication", TypeName = "varchar")]
+    [MaxLength(100)]
+    public string TypeOfMedication { get; set; } = "none";
+
+    [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+    public DateTime LastUpdate { get; set; } = new(2000, 12, 30);
+}
